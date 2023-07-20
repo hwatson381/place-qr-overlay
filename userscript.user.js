08c88a3bfd812a74994213ca7c1e2901bed28726
hwatson381 <69080709+hwatson381@users.noreply.github.com>
2023-07-20 16:10:04 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,47 +1,41 @@
 // ==UserScript==
 // @name         r/Place Overlay
 // @namespace    https://github.com/marcus-grant/place-overlay
-// @version      1.0.1
+// @version      1.0.3
 // @description  A visual overlay to show errors in tile colors of a desired image in r/place
 // @author       github.com/marcus-grant
-// @match        https://hot-potato.reddit.com/embed*
+// @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @grant        none
-// @license      GPL-3.0
-// ==/UserScript==
-// ==UserScript==
-// @name         r/Place Overlay
-// @namespace    https://github.com/marcus-grant/place-overlay
-// @version      1.0.2
-// @description  A visual overlay to show errors in tile colors of a desired image in r/place
-// @author       github.com/marcus-grant
-// @match        https://hot-potato.reddit.com/embed*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @run-at       document-start
 // @grant        none
 // @license      GPL-3.0
 // ==/UserScript==
 
-window.addEventListener('load', main, false);
+if (window.top !== window.self) {
+    window.addEventListener('load', main, false);
+}
 
 async function main() {
-  let offsetResponse = await fetch('https://raw.githubusercontent.com/hwatson381/place-qr-overlay/main/offset.json');
-  let offsets = await offsetResponse.json();
+    let offsets = {x:0, y:0};
+    offsets.x = parseInt(prompt('enter x coord for top left corner')) - 500;
+    offsets.y = parseInt(prompt('enter y coord for top left corner')) - 500;
 
-  document
-    .getElementsByTagName("mona-lisa-embed")[0]
-    .shadowRoot
-    .children[0]
-    .getElementsByTagName("mona-lisa-canvas")[0]
-    .shadowRoot
-    .children[0]
-    .appendChild(
-      (function () {
-        const img = document.createElement("img");
-        // img.src = "https://drive.google.com/file/d/1VEhNsbR4aHDePwOLmiNlxctv39TeGnci/view?usp=sharing";
-        img.src = "https://github.com/hwatson381/place-qr-overlay/raw/main/qroverlay.png";
-        img.style = `position: absolute;left: ${offsets.x}px;top: ${offsets.y}px;image-rendering: pixelated;width: 33px;height: 33px;`;
-        console.log(img);
-        return img;
-      })());
+    //let offsetResponse = await fetch('https://raw.githubusercontent.com/hwatson381/place-qr-overlay/main/offset.json');
+    //let offsets = await offsetResponse.json();
+
+      document
+        .getElementsByTagName("garlic-bread-embed")[0]
+        .shadowRoot
+        .children[0]
+        .getElementsByTagName("garlic-bread-canvas")[0]
+        .shadowRoot
+        .children[0]
+      .appendChild(
+        (function () {
+            const img = document.createElement("img");
+            // img.src = "https://drive.google.com/file/d/1VEhNsbR4aHDePwOLmiNlxctv39TeGnci/view?usp=sharing";
+            img.src = "https://github.com/hwatson381/place-qr-overlay/raw/main/qroverlay.png";
+            img.style = `position: absolute;left: ${offsets.x}px;top: ${offsets.y}px;image-rendering: pixelated;width: 33px;height: 33px;`;
+            console.log(img);
+            return img;
+        })())
 }
